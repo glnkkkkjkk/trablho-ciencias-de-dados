5ba04480e51dc4d34f3ff6fb89872e5fbb2e9e88
gabrielalecheta <114994800+glnkkkkjkk@users.noreply.github.com>
2024-10-17 09:46:04 -0300

    Update Quantidadedeusuarios.js

--- Gabi Lecheta/graficos/Quantidadedeusuarios.js
+++ Gabi Lecheta/graficos/Quantidadedeusuarios.js
@@ -1,4 +1,4 @@
-import { getCSS } from "./common.js"
+import { getCSS, tickConfig } from "./common.js"
 async function quantidaedeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fatch(url)
@@ -24,7 +24,28 @@ async function quantidaedeUsuariosPorRede() {
             text: 'Redes sociais com mais usuários',
             x: 0,
             font: {
+                color: getCSS('--primary-color'),
+                size: 30,
                 color: getCSS('--font')
+            }
+        },
+        xaxis: {
+            tickfront: tickConfig
+            title: {
+                text: "Nome das redes",
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Bilhões de usuários ativos',
+                font: {
+                    color: getCSS("--secondary-color")
+                }
+
             }
         }
     }
