f46f97f2e4408d9b7e3601359e20766ed58ba64b
gabrielalecheta <114994800+glnkkkkjkk@users.noreply.github.com>
2024-10-24 09:16:43 -0300

    Update Quantidadedeusuarios.js

--- Gabi Lecheta/graficos/Quantidadedeusuarios.js
+++ Gabi Lecheta/graficos/Quantidadedeusuarios.js
@@ -1,59 +1,113 @@
 import { getCSS, tickConfig } from "./common.js"
-async function quantidaedeUsuariosPorRede() {
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
-    const res = await fatch(url)
+
+
+
+async function quantidadeUsuariosPorRede() {
+
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios,json'
+
+    const res = await fetch(url)
+
     const dados = await res.json()
+
     const nomeDasRedes = Object.keys(dados)
+
     const quantidadeDeUsuarios = Object.values(dados)
 
+
+
     const data = [
+
         {
-            x: nomeDasRedes,
+
+            x:nomeDasRedes,
+
             y:quantidadeDeUsuarios,
-            type: 'bar' ,
-            maker: {
+
+            type: 'bar',
+
+            marker: {
+
                 color: getCSS('--primary-color')
+
             }
+
         }
+
     ]
 
-    const layout = {
+
+
+    const laytout = {
+
         plot_bgcolor: getCSS('--bg-color'),
+
         paper_bgcolor: getCSS('--bg-color'),
+
         title: {
+
             text: 'Redes sociais com mais usuários',
+
             x: 0,
+
             font: {
+
                 color: getCSS('--primary-color'),
+
                 size: 30,
-                color: getCSS('--font')
+
+                font: getCSS('--font')
+
             }
+
         },
+
         xaxis: {
-            tickfront: tickConfig
-            title: {
-                text: "Nome das redes",
-                font: {
-                    color: getCSS('--secondary-color')
-                }
-            }
-        },
-        yaxis: {
+
             tickfont: tickConfig,
+
             title: {
-                text: 'Bilhões de usuários ativos',
+
+                text:'Nome das redes',
+
                 font: {
-                    color: getCSS("--secondary-color")
+
+                    color: getCSS('--secondary-color')
+
                 }
 
             }
+
+        },
+
+        yaxis: {
+
+            tickfont: tickConfig,
+
+            title: {
+
+                text: 'Bilhões de usuários ativos',
+
+                font: {
+
+                    color: getCSS('--secondary-color')
+
+                }
+
+            }
+
         }
+
     }
 
-    const grafico = document.createElement('div')
-    grafico.className = 'grafico'
-    document.getElementById('garfico-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data, layout)
-}
 
-quantidaedeUsuariosPorRede()
+
+    const grafico = document.createElement('div')
+
+    grafico.className = 'grafico'
+
+    documen.getElementById('graficos-container').appendChild(grafico)
+
+    Plotly.newPlot(grafico, data, laytout)
+
+}
